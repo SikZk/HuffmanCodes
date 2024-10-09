481607613a305c1b9bd57b29520d01d7d18528a9
Mikołaj Truszczyński <m.truszczynski04@gmail.com>
2024-10-09 22:39:07 +0200

    added priority queue, and made frequency table, I need to make code table, and decode function

--- huffman_encoder.c
+++ huffman_encoder.c
@@ -1,24 +1,82 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define REQUIRED_ARGUMENTS_NUMBER 3
 #define ASCII_TABLE_SIZE 256
 
-struct huffman_node {
+
+typedef struct huffman_node {
     char character;
     int frequency;
     struct huffman_node *left;
     struct huffman_node *right;
-};
+} huffman_node;
 
+typedef struct {
+    huffman_node items[ASCII_TABLE_SIZE];
+    int size;
+} priority_queue;
 
-int frequency_table[ASCII_TABLE_SIZE];
+int *frequency_table;
 FILE *input_file;
 FILE *output_file;
 
 void print_help_message() {
     fprintf(stderr, "To properly use this executable you should call:\n./executable <encode/decode> <InputFile> <OutputFile>\n");
 }
+void swap_nodes(huffman_node *a, huffman_node *b) {
+    huffman_node temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void heapify_up(priority_queue *queue, int index) {
+    if (index != 0 && queue->items[(index - 1) / 2].frequency > queue->items[index].frequency) {
+        swap_nodes(&queue->items[(index - 1) / 2], &queue->items[index]);
+        heapify_up(queue, (index -1) / 2);
+    }
+}
+void heapify_down(priority_queue *queue, int index) {
+    int current_index = index;
+    const int left_child_index = 2 * index + 1;
+    const int right_child_index = 2 * index + 2;
+
+    if(left_child_index < queue->size && queue->items[left_child_index].frequency < queue->items[current_index].frequency) {
+        current_index = left_child_index;
+    }
+    if(right_child_index < queue->size && queue->items[right_child_index].frequency < queue->items[current_index].frequency) {
+        current_index = right_child_index;
+    }
+    if(current_index != index) {
+        swap_nodes(&queue->items[index], &queue->items[current_index]);
+        heapify_down(queue, current_index);
+    }
+}
+int enqueue(priority_queue *queue, huffman_node node) {
+    if (queue->size == ASCII_TABLE_SIZE) {
+        return 1;
+    }
+    queue->items[queue->size++] = node;
+    heapify_up(queue, queue->size - 1);
+    return 0;
+}
+
+huffman_node dequeue(priority_queue *queue) {
+    if (!queue->size) {
+        return (huffman_node) {0};
+    }
+    huffman_node node = queue->items[0];
+    queue->items[0] = queue->items[--queue->size];
+    heapify_down(queue, 0);
+    return node;
+}
+huffman_node peek(priority_queue *queue) {
+    if (!queue->size) {
+        return (huffman_node) {0};
+    }
+    return queue->items[0];
+}
+
 int validate_txt_file(const char *file_path) {
 
     const char *dot = strrchr(file_path, '.');
@@ -34,7 +92,6 @@ int validate_txt_file(const char *file_path) {
         perror("Error opening .txt file");
         return 1;
     }
-
     return 0;
 
 }
@@ -53,18 +110,63 @@ int validate_huff_file(const char *file_path) {
         perror("Error opening .huff file");
         return 1;
     }
-
     return 0;
 
 }
+int *create_frequency_table(FILE *input_file) {
+
+    int *table = calloc(ASCII_TABLE_SIZE, sizeof(int));
+    if (table == NULL) {
+        perror("Memory allocation failed");
+        return NULL;
+    }
+    int ch;
+    while ((ch = fgetc(input_file)) != EOF) {
+        table[ch]++;
+    }
+
+    return table;
+}
+void create_huffman_tree(priority_queue *queue) {
+    while (queue->size > 1) {
+        huffman_node lowest = dequeue(queue);
+        huffman_node second_lowest = dequeue(queue);
+        huffman_node new_node = {'\0', lowest.frequency + second_lowest.frequency, &lowest, &second_lowest};
+        enqueue(queue, new_node);
+    }
+}
+
 
 int huffman_encode(FILE *input_file, FILE *output_file) {
+    priority_queue *queue = malloc(sizeof(priority_queue));
+    if (queue == NULL) {
+        return 1;
+    }
+    frequency_table = create_frequency_table(input_file);
+    if (frequency_table == NULL) {
+        return 1;
+    }
+    for(int i = 0; i<ASCII_TABLE_SIZE; i++) {
+        if(frequency_table[i] > 0) {
+            huffman_node node = {i, frequency_table[i], NULL, NULL};
+            enqueue(queue, node);
+        }
+    }
+    free(frequency_table);
+
+    create_huffman_tree(queue);
+    huffman_node root = dequeue(queue);
+    free(queue);
+
 
 
     fclose(input_file);
     fclose(output_file);
     return 0;
 }
+
+
+
 
 int main(const int argc, char *argv[]) {
 
@@ -87,6 +189,7 @@ int main(const int argc, char *argv[]) {
             fprintf(stderr, "Error encoding the file\n");
             return 1;
         }
+
 
 
     } else if(strcmp(argv[1], "decode") == 0) {
